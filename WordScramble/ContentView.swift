996b220d8b4a65e77196342c58ebe72a5ad0b9c8
John Martin <johndavidfive@gmail.com>
2022-09-16 16:02:15 -0700

    Initial Commit

---
+++ WordScramble/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by john martin on 9/16/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by john martin on 9/16/22.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
